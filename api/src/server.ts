dbb90e8f1abea8df7f128a222215b4d50f22f914
aramis.seitz <aramis.seitz@telekom.de>
2026-04-15 12:22:26 +0000

    server ts changed for vercel deployment

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,35 +1,14 @@
 import express from "express";
-import path from "path";
 import { getSecureRandomNumber } from "./utils/random";
 
-
 const app = express();
-const PORT = 3000;
+
 const MIN_ROTATION_DEGREE: number = 140;
 const MAX_ROTATION_DEGREE: number = 900;
 
-
-
-// Pfad zum public
-const publicPath = path.join(__dirname, "../../public");
-
-// Statische Dateien 
-app.use(express.static(publicPath));
-
-//  API
-
 app.get("/api/random", (req, res) => {
     const ranNum = getSecureRandomNumber(MIN_ROTATION_DEGREE, MAX_ROTATION_DEGREE);
-
     res.json({ ranNum });
 });
- 
- 
-// Fallback: index.html laden
-app.get("*", (req, res) => {
-    res.sendFile(path.join(publicPath, "index.html"));
-});
 
-app.listen(PORT, () => {
-    console.log(`Server läuft auf http://localhost:${PORT}`);
-});
+export default app;
