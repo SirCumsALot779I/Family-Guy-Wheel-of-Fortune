175ec0a8d3f40d22571482e5a8ba783e01b27224
Winter <jarne.winter@telekom.de>
2026-04-20 12:21:03 +0200

    #69 local development brach created and setup

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,15 +1,36 @@
 import express from "express";
-import { getSecureRandomNumber } from "./utils/random.js";
+import path from "path";
+import { getSecureRandomNumber } from "./utils/random";
+
 
 const app = express();
-
+const PORT = 3000;
 const MIN_ROTATION_DEGREE: number = 140;
 const MAX_ROTATION_DEGREE: number = 900;
 
-app.get("/dist/api/random.js", (req, res) => {
+
+
+// Pfad zum Frontend
+const frontendPath = path.join(__dirname, "../../public");
+
+// Statische Dateien 
+app.use(express.static(frontendPath));
+
+//  API
+
+app.get("/api/random", (req, res) => {
     const ranNum = getSecureRandomNumber(MIN_ROTATION_DEGREE, MAX_ROTATION_DEGREE);
+
     res.json({ ranNum });
 });
+ 
+ 
+// Fallback: index.html laden
+app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+});
 
-export default app;
-/*wie kann diese änderung nicht auf dem repo sein hää?*/
+app.listen(PORT, () => {
+    console.log(`Server läuft auf http://localhost:${PORT}`);
+});
+
