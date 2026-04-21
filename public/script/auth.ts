817cb805b00483e2c9214e8daf7193cacc1c0c8f
aramis.seitz <aramis.seitz@telekom.de>
2026-04-21 11:45:33 +0000

    test ich kann nicht mehr ahhh

--- public/script/auth.ts
+++ public/script/auth.ts
@@ -40,7 +40,7 @@ if (loginForm) {
             }
 
             showMessage('Login erfolgreich!');
-            window.location.href = '/main.html';
+            window.location.href = '/';
         } catch (err: unknown) {
             console.error('Netzwerkfehler beim Login:', err);
             showMessage('Netzwerkfehler. Bitte versuchen Sie es später erneut.');
