a357d53a3c0a8fca5fd2f984251cac8fd9efe7c1
lectus76 <winterjarne@gmail.com>
2026-04-29 16:44:48 +0200

    refactor: vite.config.ts richtiger root gesetzt

--- public/vite.config.ts
+++ public/vite.config.ts
@@ -5,12 +5,12 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        index: resolve(__dirname, 'public/html/index.html'),
-        main: resolve(__dirname, 'public/html/main.html'),
-        DaS: resolve(__dirname, 'public/html/datenschutz.html'),
-        login: resolve(__dirname, 'public/html/login.html'),
-        signup: resolve(__dirname, 'public/html/signup.html'),
-        Imp: resolve(__dirname, 'public/html/impressum.html')
+        index: resolve(__dirname, 'html/index.html'),
+        main: resolve(__dirname, 'html/main.html'),
+        DaS: resolve(__dirname, 'html/privacy.html'),
+        login: resolve(__dirname, 'html/login.html'),
+        signup: resolve(__dirname, 'html/signup.html'),
+        Imp: resolve(__dirname, 'html/imprint.html')
       },
     },
   },
